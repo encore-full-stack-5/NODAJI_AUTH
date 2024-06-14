972053344768c03975fe1a8c620a32830e8c76dd
giraffenostrils <dreamthre@gamil.com>
2024-06-14 14:29:43 +0900

    FEAT:user guard 설정(#4)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,33 +5,38 @@ import {
   HttpCode,
   HttpStatus,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { RequestDTO } from './dto/RequestDTO.dto';
 import { UsersService } from './users.service';
+import { UserGuard } from './users.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
-
   @HttpCode(HttpStatus.OK)
+  @UseGuards(UserGuard)
   @Get()
   async getAllUser() {
     const users = await this.userService.getAllUsers();
     return users;
   }
   @HttpCode(HttpStatus.OK)
+  @UseGuards(UserGuard)
   @Get('id')
   async findById(@Body() id: string) {
     const users = await this.userService.findById(id);
     return users;
   }
   @HttpCode(HttpStatus.OK)
+  @UseGuards(UserGuard)
   @Get('password')
   async findByPassword(@Body() req: RequestDTO) {
     const users = await this.userService.findByPassword(req);
     return users;
   }
   @HttpCode(HttpStatus.OK)
+  @UseGuards(UserGuard)
   @Get('email')
   async findByEmail(@Body() req: RequestDTO) {
     const users = await this.userService.findByEmail(req);
@@ -44,6 +49,7 @@ export class UsersController {
     return user;
   }
   @HttpCode(HttpStatus.OK)
+  @UseGuards(UserGuard)
   @Post('password')
   async changePassword(@Body() req: RequestDTO) {
     const sucess = await this.userService.changePassword(req);
--- src/users/users.guard.ts
+++ src/users/users.guard.ts
@@ -1,11 +1,37 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { jwtConstants } from 'src/jwt/jwtConstants';
+import { Request } from 'express'; // <-- Import the Request type from express
 
 @Injectable()
-export class UsersGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+export class UserGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<Request>(); // <-- Ensure the correct type
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: jwtConstants.secret,
+      });
+
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
     return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
   }
 }
